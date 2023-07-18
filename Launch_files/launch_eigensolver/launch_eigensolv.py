40fb0c1d4689d30145082da5edfecdb9ede9f089
Luis Carlos Herrera Quesada <127136402+Lherrera094@users.noreply.github.com>
2023-07-18 07:14:24 -0600

    Add files via upload

--- Launch_files/launch_eigensolver/launch_eigensolv.py
+++ Launch_files/launch_eigensolver/launch_eigensolv.py
@@ -1,21 +1,21 @@
 #!/usr/bin/env python
 # coding: utf-8
-
+# Developer: Luis Carlos Herrera Quesada
+# Date: 10/04/2023
+# Universidad Carlos III de Madrid
 
 import sys 
 sys.path.append('FAR3_libraries/')
-from launch_eigenfunctions_TK import *
+from launch_eigensolver_TK import *
 
-launch_num = 16 #number of simulations to launch
+launch_num = 1 #number of simulations to launch
+
+sh_name = "TJII_eigen.sh"
 
 act_dir = os.listdir()
 files = sorted(list(filter(lambda act_dir: "efast" in act_dir, act_dir)))
 
-om_0, gamma = get_values_df(files)
-
-print(om_0, gamma)
-
-#done = launch_sims(files,launch_num)
+done = launch_sims(files,launch_num, sh_name)
 
 
 
--- Launch_files/launch_eigensolver/launch_eigensolver_TK.py
+++ Launch_files/launch_eigensolver/launch_eigensolver_TK.py
@@ -5,6 +5,7 @@
 # Date: 10/04/2023
 # Universidad Carlos III de Madrid
 # %%
+
 import os 
 import subprocess
 import pandas as pd
@@ -14,25 +15,14 @@ def run_files(entry,sim):
 	subprocess.call("qsub "+entry, shell = True,cwd=sim+"/")
 	
 
-def launch_sims(files,launch_num): 
+def launch_sims(files,launch_num,sh_name): 
     count = 0
     
     for i in range(len(files)):
         file = os.listdir(files[i])
-        bash = list(filter(lambda file: ".sh" in file, file))[0]
-        if "farprt" not in file and count < launch_num:
+        bash = list(filter(lambda file: "eigen.sh" in file, file))[0]
+        print(bash)
+        if f"{sh_name}" in file and count < launch_num:
             run_files(bash,files[i])
             count += 1
-
-def get_values_df(files):
-
-    for file in files:
-        if "Output_" and ".xlsx" in file:
-            df = file
-
-    dataframe = pd.read_csv(df)
-    om_0 = dataframe["Frequency"]
-    gamma = dataframe["Growth Rate"]
-
-    return om_0, gamma
 
