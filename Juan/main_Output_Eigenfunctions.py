56790a470c5f5940fb25e63e422ff14a78e418c4
Luis Carlos Herrera Quesada <127136402+Lherrera094@users.noreply.github.com>
2023-11-12 12:45:50 -0600

    Add files via upload

--- Juan/main_Output_Eigenfunctions.py
+++ Juan/main_Output_Eigenfunctions.py
@@ -14,7 +14,7 @@ from plot_functions_TK import *
 
 
 #Name the Output file as needed 
-profile_name = "Profile"
+profile_name = "New_Profile"
 
 #Give the maximum number of poloidal modes
 num_poloidal = 10
@@ -22,19 +22,23 @@ num_poloidal = 10
 l = 0
 
 #Loop to read all files from pathhhh
-results, files, txt,saving_file = create_files()
+results, files, saving_file = create_files()
 
 saving_file_Potential = "Eigenfunction_squaredPotential_plots/" #Folder to save the eigenfunctions
 if not os.path.exists(saving_file_Potential):
         os.makedirs(saving_file_Potential)
 
 for file in files:
-    
+        
     try:
         #read data
         df = pd.read_csv(file + "/phi_0000", sep="\t")
+        main_file = os.listdir(file)
+        txt = list(filter(lambda main_file: ".txt" in main_file, main_file))[0]
         r = df["r"]
         df = df.drop("r",axis=1)
+
+        print(txt,l)
     
         #Get data analysis
         print(f"Getting data from: {file}")
@@ -47,27 +51,28 @@ for file in files:
         #save data in dataframe
         results.loc[l] = [beta, round(energy), dominant_mode, radial_pos/1000, x1/1000, x2/1000, width/1000, dominant_mode_2, 
                           radial_pos_2/1000,dominant_mode_3,radial_pos_3, alfmode, gr, freq,frequency]
-        l += 1
+        
         remove_files(file)
         
         #plot phi eigenfunctions
-        exist = os.path.isfile(f"{saving_file}/{str(round(energy))}_{str(beta)}.png")
+        exist = os.path.isfile(f"{saving_file}/{str(round(energy))}_{str(beta)}_{l}.png")
         if exist == False:
             plot_eigenfunctions(dominant_mode,dominant_mode_2,dominant_mode_3,
                                 alfmode,radial_pos,radial_pos_2,radial_pos_3,df,r,
-                                energy,beta,frequency,saving_file,toroidal_coupl, num_poloidal) 
+                                energy,beta,frequency,saving_file,toroidal_coupl, num_poloidal,l) 
 
-        exist_squared = os.path.isfile(f"{saving_file_Potential}/{str(round(energy))}_{str(beta)}.png")
+        exist_squared = os.path.isfile(f"{saving_file_Potential}/{str(round(energy))}_{str(beta)}_{l}.png")
         if exist_squared == False:
             plot_squared_Potential(dominant_mode,dominant_mode_2,alfmode,
                                    radial_pos,radial_pos_2,radial_pos_3,phi_squared,r,
-                                   energy,beta,frequency,saving_file_Potential,toroidal_coupl, num_poloidal)
-        
+                                   energy,beta,frequency,saving_file_Potential,toroidal_coupl, num_poloidal, l)
+        l += 1        
+
     except Exception as e:
         print(f"Error: {e}")
         
 results.to_excel(f"Output_{profile_name}_n{toroidal_coupl[0]}.xlsx")
 
 if len(files) == len(os.listdir(saving_file)):
-    eigenfunction_maps(saving_file)
+    eigenfunction_maps(saving_file,"Eigenfunctions_full")
 
